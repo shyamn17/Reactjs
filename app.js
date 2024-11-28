8c5fff82f917a36271f112b21b095e74913e547a
shyamn17 <shyamn2001@gmail.com>
2024-11-29 00:26:30 +0530

    reactapp

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
 const heading=React.createElement("h1",{id:"title"},"hello")
 const heading1=React.createElement("h1",{id:"h1"},"hi")
 const heading2=React.createElement("h2",{id:"h2"},"bye")
@@ -5,4 +8,4 @@ const div=React.createElement("div",{id:"div"},[heading1,heading2])
 console.log(heading);
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(div)
+root.render(div);
