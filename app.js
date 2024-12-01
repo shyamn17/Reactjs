04d6fbd8d94890d4a419a3e1862fd22d35324004
shyamn17 <shyamn2001@gmail.com>
2024-12-01 21:51:05 +0530

    jsx

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  
-const heading=React.createElement("h1",{id:"title"},"hello")
-const heading1=React.createElement("h1",{id:"h1"},"hi")
-const heading2=React.createElement("h2",{id:"h2"},"bye")
-const div=React.createElement("div",{id:"div"},[heading1,heading2])
-console.log(heading);
+// const heading=React.createElement("h1",{id:"title"},"hello")
+// const heading1=React.createElement("h1",{id:"h1"},"hi")
+// const heading2=React.createElement("h2",{id:"h2"},"bye")
+// const div=React.createElement("div",{id:"div"},[heading1,heading2])
+// console.log(heading);
+
+// JSX => React.createElement => Object => HTML(DOM) 
+const heading=( 
+<h1>hello</h1>
+    );
+
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading);
+
+//function based components
+const Title=()=>( 
+  <h1>hii</h1>
+      );
+
+const Div1=()=>{
+   return(
+    <div>
+    {heading}
+    {Title()} 
+    {<Title/>} 
+    {/* both renders same */}
+     <h5>fun based component</h5>
+     </div>
+   );
+} 
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(div);
+root.render(<Div1/>);
